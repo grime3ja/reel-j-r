171ef48ac9c3b148090569ceb408a24712dc5b68
Jacob Grimes <grime3ja@dukes.jmu.edu>
2024-04-14 16:42:27 -0400

    function to change the gif on the home screen, default gif when the page is loaded

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,27 @@ function search() {
     //placeholder for search button functionality.
 }
 
+async function changeGif(searchTerm) {
+    let key = "GfikMeEJM8FxK5c55WN20VdX1OiG564a";
+    let limitNumber = 1;
+    let queryString = new URLSearchParams({api_key: key, q: searchTerm, limit: limitNumber, rating: "g", rating: "pg", rating: "pg-13"});
+    const options = {
+        method: "GET",
+        query: queryString,
+    }
+    const url = 'https://corsproxy.io/?' + encodeURIComponent("https://api.giphy.com/v1/gifs/search?" + queryString.toString());
+    let response = await fetch(url, options);
+    let image = (await response.json()).data[0].images.original.url;
+    let output = document.getElementById("figure");
+    let img = document.createElement("img");
+    img.src = image;
+    let div = document.createElement("div");
+    div.appendChild(img);
+    output.appendChild(div);
+}
+
 window.onload = ("load", async() => {
+    await changeGif("movie");
     let output = document.querySelector(".footer");
     const url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
     const options = {
